d508294142ab2a18458881e4f32358e82da67993
Pablo Ierino <pablitoierino@hotmail.com>
2017-04-08 21:30:16 +0200

    Add files via upload

---
+++ mayor.cpp
@@ -0,0 +1,16 @@
+/*
+informar mayor de dos numeros
+ Ierino, Pablo
+ k1051
+*/
+
+#include <iostream>
+int main () {
+int a; int b;
+std:: cin>> a >> b;
+if (a >= b)
+{std :: cout <<a;}
+else  {
+std:: cout << b;
+}
+}
